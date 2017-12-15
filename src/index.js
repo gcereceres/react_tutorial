1beb4eef4039e609bdac5eaba21b4d33e053fc45
Gustavo Cereceres <gcereceres@gmail.com>
2017-12-15 10:35:10 -0700

    Refactor Board render method to get rid of hard coded elements

--- src/index.js
+++ src/index.js
@@ -21,23 +21,20 @@ class Board extends React.Component {
 
   render() {
 
+    const squares = ()=>{    
+      return (
+        [0,1,2].map((i)=>{
+          return <div className="board-row">
+            {[0,3,6].map((j) => {
+              return this.renderSquare(i+j)
+            })} 
+          </div>
+        })
+      )
+    };
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {squares()}
       </div>
     );
   }
